ef89d8566bc2c2b73a849f358e39fde0aada5675
bielakarolina <bielakarolina77@gmail.com>
2016-12-02 20:13:04 +0100

    Delete Artykuł.java

--- Artykuł.java
+++
@@ -1,6 +0,0 @@
-package pierwsza_wersja;
-
-public class Artyku³ {
-
-}					// stworzony po us³yszeniu uwagi dotycz¹cej obiektowoœci mojego programu
-					// clasa bêdzie zajmowa³a siê obs³uga artyku³ow
